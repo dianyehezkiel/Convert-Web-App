edd0fa6b577169bdee58fbba1e9df55cfbfef822
dianyehezkiel <dianyehezkiel@gmail.com>
2020-01-12 18:11:59 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -98,7 +98,7 @@ function displayIsInClientInfo() {
 function registerButtonHandlers() {
     document.getElementById('openWindowButton').addEventListener('click', function() {
         liff.openWindow({
-            url: 'https://example.herokuapp.com/', // Isi dengan Endpoint URL aplikasi web Anda
+            url: 'https://convert-heroku.herokuapp.com/', // Isi dengan Endpoint URL aplikasi web Anda
             external: true
         });
     });
